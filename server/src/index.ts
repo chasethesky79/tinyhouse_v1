f06cf0ab9cc8910c93c6c7f862f65fbc4cef55f5
Bharath Seshadri <bharathseshadri@kukubara7.local>
2021-04-19 21:14:30 -0500

    Implemented delete listing and get all listings

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,17 @@
 import express from 'express';
+import { listings } from './listings';
+import bodyParser from 'body-parser';
+
 const app = express();
 const port = 9000;
-app.get('/', (_req, res) => res.send("Hello World"));
 
 app.listen(port);
+app.use(bodyParser.json())
 
 console.log(`[app]: http://localhost:${port}`);
+app.get('/listings', (_req, res) => res.status(200).send(listings));
+app.post('/delete-listing', (req, res) => {
+    const id  = req.body.id as string;
+    const indexToDelete = listings.findIndex(listing => listing.id === id);
+    res.send(indexToDelete > -1 ? listings.splice(indexToDelete, 1) : 'Listing to delete not found');
+})
