1349753b4e2454960f00308d46eeff3c22c9b57f
Bharath Seshadri <bharathseshadri@kukubara7.local>
2021-05-04 08:30:44 -0500

    Defined types if listing fields

---
+++ server/src/graphql.ts
@@ -0,0 +1,38 @@
+import { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } from 'graphql';
+
+const Listing = new GraphQLObjectType({
+    name: 'Listing',
+    fields: {
+        id: { type: GraphQLID },
+        title: { type: GraphQLString },
+        image: { type: GraphQLString },
+        address: { type: GraphQLString },
+        price: { type: GraphQLInt },
+        numOfGuests: { type: GraphQLInt },
+        numOfBeds: { type: GraphQLInt },
+        numOfBaths: { type: GraphQLInt },
+        rating: { type: GraphQLInt }
+    }
+})
+
+const query = new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+        hello: {
+            type: GraphQLString,
+            resolve: () => 'Hello from the Query!'
+        }
+    }
+});
+
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        hello: {
+            type: GraphQLString,
+            resolve: () => 'Hello from the Mutation!'
+        }
+    }
+});
+
+export const schema = new GraphQLSchema({ query, mutation })
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,12 @@
 import express from 'express';
-import { listings } from './listings';
-import bodyParser from 'body-parser';
+import { ApolloServer } from 'apollo-server-express';
+import { schema } from './graphql';
 
 const app = express();
 const port = 9000;
 
-app.listen(port);
-app.use(bodyParser.json())
+const server = new ApolloServer({ schema });
+server.applyMiddleware({ app, path: '/api'});
+console.log(`Server started at http://localhost:${port}`);
 
-console.log(`[app]: http://localhost:${port}`);
-app.get('/listings', (_req, res) => res.status(200).send(listings));
-app.post('/delete-listing', (req, res) => {
-    const id  = req.body.id as string;
-    const indexToDelete = listings.findIndex(listing => listing.id === id);
-    res.send(indexToDelete > -1 ? listings.splice(indexToDelete, 1) : 'Listing to delete not found');
-})
+app.listen(port);
