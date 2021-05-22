55a9ade10f6d42cb3fd7717a00fff654a3153e93
Bharath Seshadri <bharathseshadri@kukubara7.local>
2021-05-22 17:45:38 -0500

    Replaced username and password from environment file

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,15 +1,11 @@
 import { MongoClient } from 'mongodb';
 import { Database } from '../lib/types';
 
-const user = 'user_001';
-const password = 'luckysesh1';
-const cluster = 'cluster0';
-
-const connectionUrl = `mongodb+srv://${user}:${password}@${cluster}.zwry6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const connectionUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.zwry6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const connectToDatabase = async (): Promise<Database> => {
-    const client = await MongoClient.connect(connectionUrl, { useNewUrlParser: true });
+    const client = await MongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true });
     const db = client.db('main');
     return {
         listings: db.collection('test_listings')
