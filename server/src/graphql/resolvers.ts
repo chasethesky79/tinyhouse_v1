feec123671c479810303b398c62aca89eaadcea1
Bharath Seshadri <bharathseshadri@kukubara7.local>
2021-05-29 15:54:57 -0500

    Implemented the dgraphQL functionality with mongoDB

--- server/src/graphql/resolvers.ts
+++ server/src/graphql/resolvers.ts
@@ -1,18 +1,24 @@
-import { listings } from '../listings';
 import { IResolvers } from 'apollo-server-express';
+import { Database, Listing } from '../lib/types';
+import { ObjectId } from "mongodb";
 
 export const resolvers: IResolvers = {
     Query: {
-        listings: () => listings
+        listings: async (_root: undefined, _args: Record<string, never>, { db }: { db: Database }): Promise<Listing[]> => {
+            return await db.listings.find({}).toArray(); 
+        }
     },
     Mutation: {
-        deleteListing: (_root: undefined, { id }: { id: string }) => {
-            for (let i =0; i <listings.length; i++) {
-                if (listings[i].id === id) {
-                    return listings.splice(i, 1)[0]
-                }
+        deleteListing: async (_root: undefined, { id }: { id: string }, { db }: { db: Database }): Promise<Listing> => {
+            const query = { _id: new ObjectId(id) };
+            const deletedObj = await db.listings.findOneAndDelete(query);
+            if (!deletedObj?.value) {
+                throw new Error('Unable to delete listing from monggodb')
             }
-            throw new Error('Unable to delete listing');
+            return deletedObj.value
         } 
+    },
+    Listing: {
+        id: (listing: Listing): string => listing._id.toString()
     }
 };
